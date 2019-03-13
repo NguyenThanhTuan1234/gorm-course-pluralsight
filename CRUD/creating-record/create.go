9680e375017fe2a0519eab18968c06ee9e350d90
nguyen thanh tuan <nguyen_thanh_tuan@mediado.local>
2019-03-13 16:27:41 +0900

    Creating records with children

--- CRUD/creating-record/create.go
+++ CRUD/creating-record/create.go
@@ -4,27 +4,52 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
+	"time"
 )
 
 func main() {
-	db, err := gorm.Open("postgres", "host=localhost user=postgres password=postgres dbname=gorm sslmode=disable")
+	db, err := gorm.Open("postgres", "host=localhost port= 5432 user=postgres password=postgres dbname=gorm sslmode=disable")
 	if err != nil {
 		panic(err.Error())
 	}
+	//defer db.Close()
 
 	db.DropTable(&User{})
 	db.CreateTable(&User{})
+	db.DropTable(&Appointment{})
+	db.CreateTable(&Appointment{})
 
-	 u := User{
-	 	FirstName: "Arthur",
-	 	LastName: "Dent",
-	 }
-	 db.Debug().Create(&u)
-	 fmt.Println(db.NewRecord(&u))
+	u := User {
+		FirstName: "Arthur",
+		LastName: "Dent",
+	}
+
+	appointments := []Appointment{
+		{Subject: "First"},
+		{Subject: "Second", Attendees: []*User{&u}},
+		{Subject: "Third"},
+	}
+
+	u.Appointments = appointments
+
+	db.Debug().Create(&u)
+	fmt.Println(db.NewRecord(&u))
+
 }
 
 type User struct {
 	gorm.Model
 	FirstName	string
 	LastName	string
+	Appointments []Appointment
+}
+
+type Appointment struct {
+	gorm.Model
+	UserID		uint
+	StartTime	*time.Time
+	Duration	uint
+	Attendees	[]*User
+	Subject		string
+	Description	string
 }
